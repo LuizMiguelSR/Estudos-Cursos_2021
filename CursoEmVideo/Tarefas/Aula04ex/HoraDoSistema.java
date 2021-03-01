a46be29ad3ec6a02e0e08903c354936f84c291ac
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2021-03-01 20:49:39 -0300

    Delete CursoEmVideo directory

--- CursoEmVideo/Tarefas/Aula04ex/HoraDoSistema.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class HoraDoSistema {
-    public static void main(String[] args) {
-        Date relogio = new Date();
-        System.out.println("A hora do sistema é");
-        System.out.println(relogio.toString());
-    }
-}
--- CursoEmVideo/Tarefas/Aula04ex/IdiomaSistema.java
+++
@@ -1,9 +0,0 @@
-import java.util.Locale;
-
-public class IdiomaSistema {
-    public static void main(String[] args) {
-        Locale idioma = Locale.getDefault();
-        System.out.println("Idioma:"+idioma.getDisplayLanguage()+""+idioma.getLanguage());
-    }
-    
-}
