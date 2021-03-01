ce478439fa2866802859cd68a1add1c4c5953b9b
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2021-03-01 20:52:39 -0300

    Add files via upload

---
+++ Java/CursoEmVideo/Aulas/Aula06/TesteTipo/src/testetipo/TesteTipo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testetipo;
+
+/**
+ *
+ * @author Luiz Miguel
+ */
+public class TesteTipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);*/
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.println(idade);
+    }
+    
+}
