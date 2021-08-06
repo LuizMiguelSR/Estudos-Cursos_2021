b5afb0de46ddd61731db28a9146e39f67c6334be
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2021-08-06 20:39:38 -0300

    Aula 20
    Trabalhando com dados #02

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/Fundamentos/inicio/dados_2.js
@@ -0,0 +1,13 @@
+console.log(19.90 * 0.6);
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno "; //String --> uma sequência de símbolos
+let categoria = "Papelaria";
+console.log("Produto: " + nome + ", Categoria: " + categoria + ", Preço: " + preco + ", Desconto: " + desconto);
+
+console.log(1 + 1);
+console.log("1" + 1);
