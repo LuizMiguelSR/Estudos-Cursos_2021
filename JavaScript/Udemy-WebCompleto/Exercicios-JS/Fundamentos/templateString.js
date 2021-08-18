7f08cf0dea3ea47b9c10cc7ff2c1271f73371712
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2021-08-18 14:53:59 -0300

    Aula 39 Trabalhando com template string

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/Fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome = "Luiz";
+const concatenacao = "Olá " + nome + "!";
+const template = `
+    Olá 
+    ${nome}!`;
+console.log(concatenacao, template);
+
+// expressões...
+console.log(`1 + 1 = ${1+1}`);
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
