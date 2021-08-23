aa4c83846c16c9abfe9a7081b74181e28d8f10a7
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2021-08-23 12:01:24 -0300

    Aula 41 JavaScript conceito de objetos

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/Fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}; // chaves representa um objeto
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = 4998.90;
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaços
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+};
+
+console.log(prod2);
