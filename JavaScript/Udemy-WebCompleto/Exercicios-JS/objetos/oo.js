57eda68fa075b628fbeaa4098fd67e05f7b26d31
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-10 12:53:07 -0300

    Princípios importantes de OO em JS

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/oo.js
@@ -0,0 +1,22 @@
+// Código não executável!!!
+
+// Procedural
+processamento(valor1, valor2, valor3);
+
+// Orientação a Objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento(); // Foco passou a ser o objeto 
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
