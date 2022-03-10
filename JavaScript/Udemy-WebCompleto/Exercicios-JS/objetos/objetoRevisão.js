e627f3598b3b52d57098648d9ddb10464cf409f3
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-10 13:38:25 -0300

    Criação literal de um objeto
    Encadeada e com arrays

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/objetoRevisão.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/objetoRevisão.js
@@ -10,3 +10,25 @@ delete produto.preco;
 delete produto['marca do produto'];
 console.log(produto);
 
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function(){
+        // ...
+    }
+};
