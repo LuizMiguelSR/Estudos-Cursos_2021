6688e26e76420ccda338c6e8fefca996b219c57c
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-11 11:22:01 -0300

    Usando funções construtoras

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/criandoObjetos.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/criandoObjetos.js
@@ -6,3 +6,16 @@ console.log(obj1);
 console.log(typeof Object, typeof new Object);
 const objt2 = new Object;
 console.log(objt2);
+
+// Funções construtoras
+function Produto (nome, preco, desc){
+    this.nome = nome;
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc);
+    }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15);
+const p2 = new Produto('Notebook', 2998.99, 0.25);
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto());
+
