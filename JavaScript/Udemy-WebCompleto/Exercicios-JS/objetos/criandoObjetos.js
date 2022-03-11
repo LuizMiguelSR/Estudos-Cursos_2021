c5811959f45a3302f884387730d41b5810369006
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-11 11:22:19 -0300

    Através da função factory

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/criandoObjetos.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/criandoObjetos.js
@@ -19,3 +19,19 @@ const p1 = new Produto('Caneta', 7.99, 0.15);
 const p2 = new Produto('Notebook', 2998.99, 0.25);
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto());
 
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) * (30 - faltas);
+        }
+    }
+}
+
+const f1 = criarFuncionario('João', 7980, 4);
+const f2 = criarFuncionario('Maria', 11400, 1);
+console.log(f1.getSalario(), f2.getSalario());
+
