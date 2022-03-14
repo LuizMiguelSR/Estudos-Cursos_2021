dace523f38d0340941ab5da693e959426c456502
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-14 10:20:45 -0300

    Utilizando o object.freeze

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/objetosConstantes.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/objetosConstantes.js
@@ -6,3 +6,12 @@ console.log(pessoa);
 // pessoa -> 456 -> {...}
 // pessoa = {nome: 'Ana'};
 
+Object.freeze(pessoa);
+
+pessoa.nome = 'Maria';
+pessoa.end = 'Rua ABC';
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+console.log(pessoa);
+
