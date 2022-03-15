64918ff880e8495830e4a59660c885164694926a
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-15 12:11:00 -0300

    Criando atributos a partir da criação de uma variável

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/notacaoLiteral.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/notacaoLiteral.js
@@ -6,3 +6,9 @@ const obj1 = { a: a, b: b, c: c };
 const obj2 = { a, b, c };
 console.log(obj1, obj2);
 
+const nomeAttr = 'nota';
+const valorAttr = 7.87;
+
+const obj3 = {};
+obj3[nomeAttr] = valorAttr;
+console.log(obj3);
