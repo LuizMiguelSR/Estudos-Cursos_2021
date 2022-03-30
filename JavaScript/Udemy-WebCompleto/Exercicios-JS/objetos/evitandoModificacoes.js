dcaf709eb363d40a1c3ef60dc2ce0b5c590c5754
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-29 21:45:12 -0300

    Utilizando object.extensions

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/evitandoModificacoes.js
@@ -0,0 +1,10 @@
+// Object.preventExtension
+const produto = Object.preventExtensions ({
+    nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+console.log('Extensível:', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
