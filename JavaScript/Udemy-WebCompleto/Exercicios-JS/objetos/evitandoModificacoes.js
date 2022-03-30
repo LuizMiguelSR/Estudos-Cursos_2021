2e332c2ad7898807f35c4b2bbcc395350721904f
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-03-29 21:45:28 -0300

    Utilizando object.seal

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/evitandoModificacoes.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/objetos/evitandoModificacoes.js
@@ -8,3 +8,15 @@ produto.nome = 'Borracha'
 produto.descricao = 'Borracha escolar branca'
 delete produto.tag
 console.log(produto)
+
+// Object.seal
+const pessoa = { nome: 'Juliana', idade: 35}
+Object.seal(pessoa)
+console.log('Selado:', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+// Object.freeze = selado + valores constantes
