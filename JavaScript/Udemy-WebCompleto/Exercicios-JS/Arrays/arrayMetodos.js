3e897d2691810b4b21b1d397ca78a2a68d1c971c
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-04-05 20:24:23 -0300

    Alguns métodos com arrays

---
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/Arrays/arrayMetodos.js
@@ -0,0 +1,28 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o ultimo elemento do array
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemnto da lista
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// splice pode adicionar e remover elemntos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunsPilotos1 =  pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
