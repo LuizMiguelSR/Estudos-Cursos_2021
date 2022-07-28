1cae992028dd2e9a603b38d20963c888581f3b83
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-07-28 18:47:30 -0300

    Usando Promise

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/esnext/promise2.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/esnext/promise2.js
@@ -9,3 +9,14 @@ setTimeout(function() {
         })
     }, 2000)
 }, 2000)
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            console.log('Executando promise...')
+            resolve('Vishhh')
+        }, tempo)
+    })
+}
+
+esperarPor(3000).then(texto => console.log(texto))
