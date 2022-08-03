dd2a427e8dd1881568b91649553f13c082d29af9
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-08-03 20:13:08 -0300

    Utilizando Async/Await

--- JavaScript/Udemy-WebCompleto/Exercicios-JS/esnext/asyncAwait1.js
+++ JavaScript/Udemy-WebCompleto/Exercicios-JS/esnext/asyncAwait1.js
@@ -4,9 +4,43 @@ function esperarPor(tempo = 2000) {
     })
 }
 
+/*
 esperarPor(2000)
     .then(() => console.log('Executando Promise 1...'))
     .then(esperarPor)
     .then(() => console.log('Executando Promise 2...'))
     .then(esperarPor)
     .then(() => console.log('Executando Promise 3...'))
+*/
+
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 5000)
+    })
+}
+
+async function retornarValorRapido() {
+    return 20
+}
+
+async function executar() {
+    let valor = await retornarValorRapido()
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor}...`)
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor + 1}...`)
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor + 2}...`)
+
+    return valor + 3
+}
+
+async function executarDeVerdade() {
+    const valor = await executar()
+    console.log(valor)
+}
+
+executarDeVerdade()
